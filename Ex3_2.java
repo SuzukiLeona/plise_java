13729e215498ae2e94bc955e05057eb1a85858ac
SuzukiLeona <leo.suzuki0327@gmail.com>
2018-12-28 00:25:23 +0900

    3章練習問題 3.2

---
+++ Ex3_2.java
@@ -0,0 +1,17 @@
+package plise_java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex3_2 {
+	public static void main(String[] args) {
+		Hero hero1 = new Hero("斎藤");
+		Hero hero2 = new Hero("鈴木");
+		List<Hero> heroes = new ArrayList<>();
+		heroes.add(hero1);
+		heroes.add(hero2);
+		for (Hero hero : heroes) {
+			System.out.println(hero.getName());
+		}
+	}
+}
